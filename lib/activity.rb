576e0c6677ac4958034c3141a24fb87dc009a187
Megan Arellano <ssciolist@gmail.com>
2018-02-21 09:44:29 -0700

    it1 done

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,6 +1,13 @@
 class Activity
-  attr_reader :name
-  def initialize(name)
+  attr_reader :name, :participants, :cost
+  def initialize(name, participants = {}, cost)
     @name = name
+    @participants = participants
+    @cost = cost
+  end
+
+  def add_participant(name, amount_paid)
+    @participants[:name] << name
+    @participants[:amount_paid] << amount_paid
   end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -5,23 +5,56 @@ require './lib/activity'
 
 class ActivityTest < Minitest::Test
   def test_activity_exists
-    activity = Activity.new("hiking")
+    participants = {
+        name:          "Jane",
+        amount_paid:    16
+      }
+    activity = Activity.new("hiking", participants, 20)
 
     assert_instance_of Activity, activity
   end
 
   def test_activity_has_a_name
-    activity = Activity.new("hiking")
+    participants = {
+        name:          "Jane",
+        amount_paid:    16
+      }
+    activity = Activity.new("hiking", participants, 20)
 
     assert_equal "hiking", activity.name
   end
 
   def test_activity_has_participants
+    participants = {
+        name:          "Jane",
+        amount_paid:    16
+      }
+    activity = Activity.new("hiking", participants, 20)
+
+    assert_equal "Jane", activity.participants[:name]
+
   end
 
   def test_participants_can_be_added_to_activity
+    participants = {
+        name:          ["Jane"],
+        amount_paid:    [16]
+      }
+    activity = Activity.new("hiking", participants, 20)
+
+    activity.add_participant("Megan", 5)
+
+    assert_equal "Jane", activity.participants[:name][0]
+    assert_equal "Megan", activity.participants[:name][1]
   end
 
   def test_activity_has_total_cost
+    participants = {
+        name:          ["Jane"],
+        amount_paid:    [16]
+      }
+    activity = Activity.new("hiking", participants, 20)
+
+    assert_equal 20, activity.cost
   end
 end
