9f9f0b12a02a96a4113322dbcb035a1dcf1dd08d
Megan Arellano <ssciolist@gmail.com>
2018-02-21 09:18:01 -0700

    adds failing tests

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,25 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_activity_exists
+    activity = Activity.new("hiking")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_activity_has_a_name
+    skip
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+  
+  def test_activity_has_participants
+  end
+
+  def test_participants_can_be_added_to_activity
+  end
+
+  def test_activity_has_total_cost
+  end
 end
